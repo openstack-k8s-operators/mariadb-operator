openstack-k8s-operators/mariadb-operator

commit 5d6e42484c9ba626b014c6ee6bb469b76a5c789b

Use finalizer to cleanup DB on CR delete
Introduces a finalizer to run a DB delete job on CR delete to be
able to cleanup also DB from other operators.

controllers/mariadbdatabase_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -27,6 +28,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	util "github.com/openstack-k8s-operators/lib-common/pkg/util"
 	databasev1beta1 "github.com/openstack-k8s-operators/mariadb-operator/api/v1beta1"
@@ -54,14 +56,11 @@ func (r *MariaDBDatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, er
 	instance := &databasev1beta1.MariaDBDatabase{}
 	err := r.Client.Get(context.TODO(), req.NamespacedName, instance)
 	if err != nil {
-		if k8s_errors.IsNotFound(err) {
-			// Request object not found, could have been deleted after reconcile request.
-			// Owned objects are automatically garbage collected.
-			// For additional cleanup logic use finalizers. Return and don't requeue.
-			return ctrl.Result{}, nil
-		}
 		// Error reading the object - requeue the request.
-		return ctrl.Result{}, err
+		// ignore not found errors, since they can't be fixed by an immediate
+		// requeue, and we can get them on deleted requests which we now
+		// handle using finalizer.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
 	// Fetch the MariaDB instance from which we'll pull the credentials
@@ -76,8 +75,57 @@ func (r *MariaDBDatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, er
 	if err != nil {
 		if k8s_errors.IsNotFound(err) {
 			r.Log.Info("No DB found for label 'dbName'.")
+			return ctrl.Result{}, nil
 		}
 		return ctrl.Result{RequeueAfter: time.Second * 10}, err
+	}
+
+	finalizerName := "mariadb-" + instance.Name
+	// if deletion timestamp is set on the instance object, the CR got deleted
+	if instance.ObjectMeta.DeletionTimestamp.IsZero() {
+		// if it is a new instance, add the finalizer
+		if !controllerutil.ContainsFinalizer(instance, finalizerName) {
+			controllerutil.AddFinalizer(instance, finalizerName)
+			err = r.Client.Update(context.TODO(), instance)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			r.Log.Info(fmt.Sprintf("Finalizer %s added to CR %s", finalizerName, instance.Name))
+		}
+
+	} else {
+		// 1. check if finalizer is there
+		// Reconcile if finalizer got already removed
+		if !controllerutil.ContainsFinalizer(instance, finalizerName) {
+			return ctrl.Result{}, nil
+		}
+
+		// 2. delete the database
+		r.Log.Info(fmt.Sprintf("CR %s delete, running DB delete job", instance.Name))
+		job := mariadb.DeleteDbDatabaseJob(instance, db.Name, db.Spec.Secret, db.Spec.ContainerImage)
+
+		requeue, err := util.EnsureJob(job, r.Client, r.Log)
+		if err != nil {
+			return ctrl.Result{}, err
+		} else if requeue {
+			r.Log.Info("Waiting on DB delete")
+			return ctrl.Result{RequeueAfter: time.Second * 5}, err
+		}
+
+		// delete the job
+		requeue, err = util.DeleteJob(job, r.Kclient, r.Log)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		// 3. as last step remove the finalizer on the operator CR to finish delete
+		controllerutil.RemoveFinalizer(instance, finalizerName)
+		err = r.Client.Update(context.TODO(), instance)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		r.Log.Info(fmt.Sprintf("CR %s deleted", instance.Name))
+		return ctrl.Result{}, nil
 	}
 
 	if db.Status.DbInitHash == "" {

pkg/database.go
@@ -69,3 +69,48 @@ func DbDatabaseJob(database *databasev1beta1.MariaDBDatabase, databaseHostName s
 
 	return job
 }
+
+func DeleteDbDatabaseJob(database *databasev1beta1.MariaDBDatabase, databaseHostName string, databaseSecret string, containerImage string) *batchv1.Job {
+
+	opts := dbCreateOptions{database.Spec.Name, databaseHostName, "root"}
+	labels := map[string]string{
+		"owner": "mariadb-operator", "cr": database.Spec.Name, "app": "mariadbschema",
+	}
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      database.Spec.Name + "-database-delete",
+			Namespace: database.Namespace,
+			Labels:    labels,
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					RestartPolicy:      "OnFailure",
+					ServiceAccountName: "mariadb",
+					Containers: []corev1.Container{
+						{
+							Name:    "mariadb-database-create",
+							Image:   containerImage,
+							Command: []string{"/bin/sh", "-c", util.ExecuteTemplateFile("delete_database.sh", &opts)},
+							Env: []corev1.EnvVar{
+								{
+									Name: "MYSQL_PWD",
+									ValueFrom: &corev1.EnvVarSource{
+										SecretKeyRef: &corev1.SecretKeySelector{
+											LocalObjectReference: corev1.LocalObjectReference{
+												Name: databaseSecret,
+											},
+											Key: "DbRootPassword",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return job
+}

templates/delete_database.sh
@@ -1,0 +1,3 @@
+#!/bin/bash
+
+mysql -h {{.DatabaseHostname}} -u {{.DatabaseAdminUsername}} -P 3306 -e "DROP DATABASE IF EXISTS {{.DatabaseName}}; DROP USER IF EXISTS '{{.DatabaseName}}'@'localhost'; DROP USER IF EXISTS '{{.DatabaseName}}'@'%';"

commit 5fa1841f1209628deb2e1530c643f1337fb0277f

Fix race with MariaDB/MariaDBDatabase
This resolves a race introduced in 5d6e4248 where MariaDBDatabase
objects might not get created unless the MariaDB already exists.
The issue was that reconcile was returning a nil result (thus
signalling completion) when it was supposed to be blocking
on the MariaDB to be created.

controllers/mariadbdatabase_controller.go
@@ -75,11 +75,10 @@ func (r *MariaDBDatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, er
 	objectKey, err := client.ObjectKeyFromObject(db)
 	err = r.Client.Get(context.TODO(), objectKey, db)
 	if err != nil {
-		if k8s_errors.IsNotFound(err) {
-			r.Log.Info("No DB found for label 'dbName'.")
-			return ctrl.Result{}, nil
+		if !k8s_errors.IsNotFound(err) {
+			return ctrl.Result{}, err
 		}
-		return ctrl.Result{RequeueAfter: time.Second * 10}, err
+		return ctrl.Result{RequeueAfter: time.Second * 10}, nil
 	}
 
 	finalizerName := "mariadb-" + instance.Name

controllers/suite_test.go
@@ -29,7 +29,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	// logf "sigs.k8s.io/controller-runtime/pkg/log"
 	// "sigs.k8s.io/controller-runtime/pkg/log/zap"
-
 	// databasev1beta1 "github.com/openstack-k8s-operators/mariadb-operator/api/v1beta1"
 	// +kubebuilder:scaffold:imports
 )
@@ -48,6 +47,7 @@ func TestAPIs(t *testing.T) {
 		"Controller Suite",
 		[]Reporter{printer.NewlineReporter{}})
 }
+
 /*
 var _ = BeforeSuite(func(done Done) {
 	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))