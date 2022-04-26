f32af538099ba5f9f349f92724db2ca35705d931
Dan Prince <dprince@redhat.com>
2022-04-26 13:12:36 -0400

    Copy controllers from CNOSP

--- controllers/mariadb_controller.go
+++ controllers/mariadb_controller.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022.
+
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,45 +18,192 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"reflect"
+	"time"
 
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	mariadbv1beta1 "github.com/openstack-k8s-operators/mariadb-operator/api/v1beta1"
+	util "github.com/openstack-k8s-operators/lib-common/pkg/util"
+	databasev1beta1 "github.com/openstack-k8s-operators/mariadb-operator/api/v1beta1"
+	mariadb "github.com/openstack-k8s-operators/mariadb-operator/pkg"
+	"k8s.io/client-go/kubernetes"
 )
 
 // MariaDBReconciler reconciles a MariaDB object
 type MariaDBReconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
+	Client  client.Client
+	Kclient kubernetes.Interface
+	Log     logr.Logger
+	Scheme  *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=mariadb.openstack.org,resources=mariadbs,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=mariadb.openstack.org,resources=mariadbs/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=mariadb.openstack.org,resources=mariadbs/finalizers,verbs=update
+// +kubebuilder:rbac:groups=database.openstack.org,resources=mariadbs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=database.openstack.org,resources=mariadbs/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;create;update;delete;
+// +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;create;update;delete;
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;create;update;delete;
+// +kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=get;list;create;update;delete;
+// +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;create;update;delete;
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the MariaDB object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
+// Reconcile reconcile mariadb API requests
 func (r *MariaDBReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	_ = r.Log.WithValues("mariadb", req.NamespacedName)
 
-	// TODO(user): your logic here
+	// Fetch the MariaDB instance
+	instance := &databasev1beta1.MariaDB{}
+	err := r.Client.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
+		if k8s_errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers. Return and don't requeue.
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return ctrl.Result{}, err
+	}
+
+	// PVC
+	pvc := mariadb.Pvc(instance, r.Scheme)
+
+	foundPvc := &corev1.PersistentVolumeClaim{}
+	err = r.Client.Get(context.TODO(), types.NamespacedName{Name: pvc.Name, Namespace: pvc.Namespace}, foundPvc)
+	if err != nil && k8s_errors.IsNotFound(err) {
+
+		r.Log.Info("Creating a new Pvc", "PersistentVolumeClaim.Namespace", pvc.Namespace, "Service.Name", pvc.Name)
+		err = r.Client.Create(context.TODO(), pvc)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		return ctrl.Result{RequeueAfter: time.Second * 5}, err
+	} else if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	service := mariadb.Service(instance, r.Scheme)
+
+	// Check if this Service already exists
+	foundService := &corev1.Service{}
+	err = r.Client.Get(context.TODO(), types.NamespacedName{Name: service.Name, Namespace: service.Namespace}, foundService)
+	if err != nil && k8s_errors.IsNotFound(err) {
+
+		r.Log.Info("Creating a new Service", "Service.Namespace", service.Namespace, "Service.Name", service.Name)
+		err = r.Client.Create(context.TODO(), service)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		return ctrl.Result{RequeueAfter: time.Second * 5}, err
+	} else if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// ConfigMap
+	configMap := mariadb.ConfigMap(instance, r.Scheme)
+	// Check if this ConfigMap already exists
+	foundConfigMap := &corev1.ConfigMap{}
+	err = r.Client.Get(context.TODO(), types.NamespacedName{Name: configMap.Name, Namespace: configMap.Namespace}, foundConfigMap)
+	if err != nil && k8s_errors.IsNotFound(err) {
+		r.Log.Info("Creating a new ConfigMap", "ConfigMap.Namespace", configMap.Namespace, "Job.Name", configMap.Name)
+		err = r.Client.Create(context.TODO(), configMap)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	} else if !reflect.DeepEqual(configMap.Data, foundConfigMap.Data) {
+		r.Log.Info("Updating ConfigMap")
+		foundConfigMap.Data = configMap.Data
+		err = r.Client.Update(context.TODO(), foundConfigMap)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{RequeueAfter: time.Second * 5}, err
+	}
+
+	configHash, err := util.ObjectHash(configMap)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("error getting configMap hash: %v", err)
+	}
+
+	// Define a new Job object
+	job := mariadb.DbInitJob(instance, r.Scheme)
+	dbInitHash, err := util.ObjectHash(job)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("error calculating DB init hash: %v", err)
+	}
+
+	requeue := true
+	if instance.Status.DbInitHash != dbInitHash {
+		requeue, err = util.EnsureJob(job, r.Client, r.Log)
+		r.Log.Info("Running DB init")
+		if err != nil {
+			return ctrl.Result{}, err
+		} else if requeue {
+			r.Log.Info("Waiting on DB init")
+			return ctrl.Result{RequeueAfter: time.Second * 5}, err
+		}
+	}
+	// db init completed... okay to store the hash to disable it
+	if err := r.setDbInitHash(instance, dbInitHash); err != nil {
+		return ctrl.Result{}, err
+	}
+	// delete the job
+	_, err = util.DeleteJob(job, r.Kclient, r.Log)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Pod
+	pod := mariadb.Pod(instance, r.Scheme, configHash)
+	// Check if this Pod already exists
+	foundPod := &corev1.Pod{}
+	err = r.Client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, foundPod)
+	if err != nil && k8s_errors.IsNotFound(err) {
+		r.Log.Info("Creating a new Pod", "Pod.Namespace", pod.Namespace, "Job.Name", pod.Name)
+		err = r.Client.Create(context.TODO(), pod)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{RequeueAfter: time.Second * 30}, err
+	} else if instance.Spec.ContainerImage != foundPod.Spec.Containers[0].Image {
+		r.Log.Info("Updating Pod...")
+		foundPod.Spec.Containers[0].Image = instance.Spec.ContainerImage
+		foundPod.Spec.InitContainers[0].Image = instance.Spec.ContainerImage
+		err = r.Client.Update(context.TODO(), foundPod)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{RequeueAfter: time.Second * 30}, err
+	}
 
 	return ctrl.Result{}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
+func (r *MariaDBReconciler) setDbInitHash(db *databasev1beta1.MariaDB, hashStr string) error {
+
+	if hashStr != db.Status.DbInitHash {
+		db.Status.DbInitHash = hashStr
+		if err := r.Client.Status().Update(context.TODO(), db); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SetupWithManager -
 func (r *MariaDBReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&mariadbv1beta1.MariaDB{}).
+		For(&databasev1beta1.MariaDB{}).
+		Owns(&corev1.Service{}).
+		Owns(&corev1.ConfigMap{}).
+		Owns(&corev1.PersistentVolumeClaim{}).
+		Owns(&corev1.Pod{}).
 		Complete(r)
 }
--- controllers/mariadbdatabase_controller.go
+++ controllers/mariadbdatabase_controller.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022.
+
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,45 +18,162 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/go-logr/logr"
+	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
-	mariadbv1beta1 "github.com/openstack-k8s-operators/mariadb-operator/api/v1beta1"
+	util "github.com/openstack-k8s-operators/lib-common/pkg/util"
+	databasev1beta1 "github.com/openstack-k8s-operators/mariadb-operator/api/v1beta1"
+	mariadb "github.com/openstack-k8s-operators/mariadb-operator/pkg"
 )
 
 // MariaDBDatabaseReconciler reconciles a MariaDBDatabase object
 type MariaDBDatabaseReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Kclient kubernetes.Interface
+	Log     logr.Logger
+	Scheme  *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=mariadb.openstack.org,resources=mariadbdatabases,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=mariadb.openstack.org,resources=mariadbdatabases/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=mariadb.openstack.org,resources=mariadbdatabases/finalizers,verbs=update
+// +kubebuilder:rbac:groups=database.openstack.org,resources=mariadbdatabases,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=database.openstack.org,resources=mariadbdatabases/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=database.openstack.org,resources=mariadbs/status,verbs=get;list
+// +kubebuilder:rbac:groups=database.openstack.org,resources=mariadbs/status,verbs=get;list
+// +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;create;update;delete;
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the MariaDBDatabase object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
+// Reconcile reconcile mariadbdatabase API requests
 func (r *MariaDBDatabaseReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	_ = r.Log.WithValues("mariadbdatabase", req.NamespacedName)
 
-	// TODO(user): your logic here
+	// Fetch the MariaDBDatabase instance
+	instance := &databasev1beta1.MariaDBDatabase{}
+	err := r.Client.Get(ctx, req.NamespacedName, instance)
+	if err != nil {
+		// Error reading the object - requeue the request.
+		// ignore not found errors, since they can't be fixed by an immediate
+		// requeue, and we can get them on deleted requests which we now
+		// handle using finalizer.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Fetch the MariaDB instance from which we'll pull the credentials
+	db := &databasev1beta1.MariaDB{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.ObjectMeta.Labels["dbName"],
+			Namespace: req.Namespace,
+		},
+	}
+	objectKey := client.ObjectKeyFromObject(db)
+	err = r.Client.Get(ctx, objectKey, db)
+	if err != nil {
+		if !k8s_errors.IsNotFound(err) {
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{RequeueAfter: time.Second * 10}, nil
+	}
+
+	finalizerName := "mariadb-" + instance.Name
+	// if deletion timestamp is set on the instance object, the CR got deleted
+	if instance.ObjectMeta.DeletionTimestamp.IsZero() {
+		// if it is a new instance, add the finalizer
+		if !controllerutil.ContainsFinalizer(instance, finalizerName) {
+			controllerutil.AddFinalizer(instance, finalizerName)
+			err = r.Client.Update(ctx, instance)
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+			r.Log.Info(fmt.Sprintf("Finalizer %s added to CR %s", finalizerName, instance.Name))
+		}
+
+	} else {
+		// 1. check if finalizer is there
+		// Reconcile if finalizer got already removed
+		if !controllerutil.ContainsFinalizer(instance, finalizerName) {
+			return ctrl.Result{}, nil
+		}
+
+		// 2. delete the database
+		r.Log.Info(fmt.Sprintf("CR %s delete, running DB delete job", instance.Name))
+		job := mariadb.DeleteDbDatabaseJob(instance, db.Name, db.Spec.Secret, db.Spec.ContainerImage)
+
+		requeue, err := util.EnsureJob(job, r.Client, r.Log)
+		if err != nil {
+			return ctrl.Result{}, err
+		} else if requeue {
+			r.Log.Info("Waiting on DB delete")
+			return ctrl.Result{RequeueAfter: time.Second * 5}, err
+		}
+
+		// delete the job
+		_, err = util.DeleteJob(job, r.Kclient, r.Log)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		// 3. as last step remove the finalizer on the operator CR to finish delete
+		controllerutil.RemoveFinalizer(instance, finalizerName)
+		err = r.Client.Update(ctx, instance)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+		r.Log.Info(fmt.Sprintf("CR %s deleted", instance.Name))
+		return ctrl.Result{}, nil
+	}
+
+	if db.Status.DbInitHash == "" {
+		r.Log.Info("DB initialization not complete. Requeue...")
+		return ctrl.Result{RequeueAfter: time.Second * 10}, err
+	}
+
+	// Define a new Job object (hostname, password, containerImage)
+	job := mariadb.DbDatabaseJob(instance, db.Name, db.Spec.Secret, db.Spec.ContainerImage)
+
+	requeue := true
+	if instance.Status.Completed {
+		requeue, err = util.EnsureJob(job, r.Client, r.Log)
+		r.Log.Info("Creating database...")
+		if err != nil {
+			return ctrl.Result{}, err
+		} else if requeue {
+			r.Log.Info("Waiting on database creation job...")
+			return ctrl.Result{RequeueAfter: time.Second * 5}, err
+		}
+	}
+	// database creation finished... okay to set to completed
+	if err := r.setCompleted(instance, ctx); err != nil {
+		return ctrl.Result{}, err
+	}
+	// delete the job
+	requeue, err = util.DeleteJob(job, r.Kclient, r.Log)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
+func (r *MariaDBDatabaseReconciler) setCompleted(db *databasev1beta1.MariaDBDatabase, ctx context.Context) error {
+
+	if !db.Status.Completed {
+		db.Status.Completed = true
+		if err := r.Client.Status().Update(ctx, db); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SetupWithManager -
 func (r *MariaDBDatabaseReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&mariadbv1beta1.MariaDBDatabase{}).
+		For(&databasev1beta1.MariaDBDatabase{}).
 		Complete(r)
 }
