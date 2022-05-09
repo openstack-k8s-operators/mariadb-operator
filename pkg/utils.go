6afba6583df3f5ddb8bee4fac4b01fa6ceb598da
Dan Prince <dprince@redhat.com>
2022-05-09 15:58:08 -0400

    Prefix cr label with mariadb

--- pkg/utils.go
+++ pkg/utils.go
@@ -2,5 +2,5 @@ package mariadb
 
 // GetLabels -
 func GetLabels(name string) map[string]string {
-	return map[string]string{"owner": "mariadb-operator", "cr": name, "app": "mariadb"}
+	return map[string]string{"owner": "mariadb-operator", "cr": "mariadb-" + name, "app": "mariadb"}
 }
