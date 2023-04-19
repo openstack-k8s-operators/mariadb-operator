10c2f31c39c079ef16b8e02a52a751b3633a6155
Damien Ciabrini <dciabrin@redhat.com>
2023-04-19 14:25:17 +0000

    galera: do not require root user to init/run pods
    The galera pods are configured to run as root so that Kolla can copy
    config files and bootstrap a database. Later, mysqld drops privileges
    by switching to user mysql via setuid().
    
    Do not explicitely require the root user and use sudo rights instead.
    This works because Kolla is already allowed to sudo, and the mysqld
    server can set limits (e.g. file descriptors, sockets) without
    requiring root privileges.

--- templates/galera/bin/mysql_probe.sh
+++ templates/galera/bin/mysql_probe.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-set -eux
+set -eu
 
 # This secret is mounted by k8s and always up to date
 read -s -u 3 3< /var/lib/secrets/dbpassword MYSQL_PWD || true
 export MYSQL_PWD
 
+PROBE_USER=root
+
 # Consider the pod has "started" once mysql is reachable
 if [ "$1" = "startup" ]; then
-    mysql -sNe "select(1);"
+    mysql -u${PROBE_USER} -sNe "select(1);"
     exit $?
 fi
+
+set -x
 
 case "$1" in
     readiness)
         # If the node is e.g. a donor, it cannot serve traffic
-        mysql -sNe "show status like 'wsrep_local_state_comment';" | grep -w -e Synced;;
+        mysql -u${PROBE_USER} -sNe "show status like 'wsrep_local_state_comment';" | grep -w -e Synced;;
     liveness)
         # If the node is not in the primary partition, restart it
-        mysql -sNe "show status like 'wsrep_cluster_status';" | grep -w -e Primary;;
+        mysql -u${PROBE_USER} -sNe "show status like 'wsrep_cluster_status';" | grep -w -e Primary;;
     *)
         echo "Invalid probe option '$1'"
         exit 1;;
