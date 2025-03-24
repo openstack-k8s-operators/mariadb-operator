fe9ddbe30c3808ea5e559f6f2fc79fb2cfb2c1e4
Mike Bayer <mbayer@redhat.com>
2025-03-24 18:39:52 -0400

    mariadbaccount system accounts
    introduce a new class of MariaDBAccount called a "system"
    MariaDBAccount, indicated by a new enumerated field AccountType
    on the CR.  Such accounts link directly to a Galera instance
    and have no dependency on a MariaDBDatabase CR.
    
    The expected targets for "system" accounts will include the
    Galera/mysql root username and password, as well as a system
    account used by mariadbbackup for SST.
    
    Refactor mariadbaccount_controller to isolate logic used for
    acquiring MariaDBDatabase and Galera CRs into separate functions,
    and ensure all MariaDBDatabase logic takes place only for "user"
    accounts (which would be all current MariaDBAccount CRs).
    
    Also correct an oversight where MariaDBAccount would not unconditionally
    apply a finalizer to its Secret object.   This logic now takes place
    in addition to an unconditional removal of the finalizer when the
    MariaDBAccount object is deleted.
    
    A subsequent change will allow system-level passwords to be changed
    in place by applying the secret name to two separate fields
    MariaDBAccount/Spec/Secret and MariaDBAccount/Status/Secret.  When
    these two names differ it will indicate an in-place password change
    should take place.

--- tests/kuttl/common/scripts/check_db_account.sh
+++ tests/kuttl/common/scripts/check_db_account.sh
@@ -26,7 +26,11 @@ fi
 
 # username was found.  if we wanted it to be found, then check the login also.
 if [ "$found" = "0" ]; then
-    oc rsh -n ${NAMESPACE} -c galera ${galera} /bin/sh -c "mysql -u${username} -p${password} -Nse 'select database();' ${dbname}" || exit -1
+    if [ -n "$dbname" ]; then
+        oc rsh -n ${NAMESPACE} -c galera ${galera} /bin/sh -c "mysql -u${username} -p${password} -Nse 'select database();' ${dbname}" || exit -1
+    else
+        oc rsh -n ${NAMESPACE} -c galera ${galera} /bin/sh -c "mysql -u${username} -p${password} -Nse 'select 1'" || exit -1
+    fi
 fi
 
 exit $found
